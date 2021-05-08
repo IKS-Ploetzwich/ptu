5e189bed5936daab793899268171b88ead87ada6
Robin Plötzwich <r.ploetzwich@iks-gmbh.com>
2021-05-08 17:12:26 +0200

    removed unnecessary folders

--- module/config.js
+++
@@ -1,7 +0,0 @@
-export const ptu = {};
-
-ptu.damageType = {
-    physical: "ptu.damageType.physical",
-    special: "ptu.damageType.special",
-    status: "ptu.damageType.status"
-}
--- module/templates.js
+++
@@ -1,5 +0,0 @@
-export const preloadHandlebarsTemplates = async () =>
-    loadTemplates([
-
-
-    ]);
